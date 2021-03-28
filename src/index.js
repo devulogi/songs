37223074e46bc6edea1241f12baf82ef06681476
devulogi <devulogijrp.github@gmail.com>
2021-03-28 12:19:26 +0800

    added styling

--- src/index.js
+++ src/index.js
@@ -8,8 +8,12 @@ import SongList from './components/SongList';
 
 const App = () => {
   return (
-    <div className="ui container">
-      <SongList />
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
     </div>
   );
 };
